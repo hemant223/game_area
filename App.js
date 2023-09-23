36115109badc7b5a78a6bfdc3fd96884d154ad64
abhinav <abhinav@plus91labs.com>
2023-09-23 17:57:49 +0530

    merging

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React,{useEffect} from 'react';
 import MainNavigationStack from './src/navigations/mainNavigationStack/MainNavigationStack';
 import {Provider} from 'react-redux';
 import store from './src/helper/utils/redux/store';
-
+// import firestore from '@react-native-firebase/firestore';
 const App = () => {
+   
+  // useEffect(() => {
+  //   firestore()
+  //   .collection('Users')
+  //   .add({
+  //     name: 'Ada Lovelace',
+  //     age: 30,
+  //   })
+  //   .then(() => {
+  //     console.log('User added!');
+  //   });
+  // }, [])
+  
+
   return (
     <Provider store={store}>
       <MainNavigationStack />
